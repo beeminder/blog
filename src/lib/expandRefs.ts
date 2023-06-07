df5596261fa1cd48cf1aad6e42aaf0b34f75ff36
Nathan Arthur <nathan@nathanarthur.com>
2023-06-07 14:09:31 -0400

    support $REF syntax

---
+++ src/lib/expandRefs.ts
@@ -0,0 +1,23 @@
+// http://expost.padm.us/
+
+function findIdentifiers(markdown: string): string[] {
+  const fns = markdown.matchAll(/\$REF\[(.*?)\]/g);
+  const ids: string[] = Array.from(fns)
+    .map((fn) => fn[1])
+    .filter((v): v is string => !!v);
+
+  return [...new Set(ids)];
+}
+
+export default function expandRefs(markdown: string): string {
+  const ids = findIdentifiers(markdown);
+
+  return ids.reduce((acc, id) => {
+    const regex = new RegExp(`\\$(?:REF\\[)?${id}\\]?`, "g");
+    const chunks = acc.split(regex);
+
+    return chunks.reduce((acc, chunk, j) =>
+      j === 0 ? chunk : `${acc}${j}${chunk}`
+    );
+  }, markdown);
+}
