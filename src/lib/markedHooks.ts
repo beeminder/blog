a3c3e3a7b9e62094d48d9f8b386efef0bea244fd
Nathan Arthur <nathan@nathanarthur.com>
2023-06-07 14:49:35 -0400

    refactor markedHooks

--- src/lib/markedHooks.ts
+++ src/lib/markedHooks.ts
@@ -1,38 +1,38 @@
 import { JSDOM } from "jsdom";
 
+function applyIdsToElements(html: string): string {
+  const dom = new JSDOM(html);
+
+  const elements = Array.from(
+    dom.window.document.querySelectorAll(
+      "*:not(script):not(noscript):not(style)"
+    )
+  ).filter((e) => {
+    return (
+      e.textContent?.length &&
+      e.children.length === 0 &&
+      /\{.*?\}/.test(e.textContent)
+    );
+  });
+
+  elements.forEach((el) => {
+    if (!el.textContent) return;
+
+    const idText = el.textContent.match(/\{#(.*)\}/)?.[1];
+    const newTextContent = el.textContent.replace(/\{#([^}]*?)\}/g, "").trim();
+
+    if (idText) {
+      el.setAttribute("id", idText);
+    }
+
+    el.textContent = newTextContent;
+  });
+
+  return dom.window.document.body.innerHTML;
+}
+
 export default {
   postprocess(html: string): string {
-    const dom = new JSDOM(html);
-
-    const elements = Array.from(
-      dom.window.document.querySelectorAll(
-        "*:not(script):not(noscript):not(style)"
-      )
-    ).filter((e) => {
-      return (
-        e.textContent?.length &&
-        e.children.length === 0 &&
-        /\{.*?\}/.test(e.textContent)
-      );
-    });
-
-    elements.forEach((el) => {
-      if (!el.textContent) return;
-
-      const idText = el.textContent.match(/\{#(.*)\}/)?.[1];
-      const newTextContent = el.textContent
-        .replace(/\{#([^}]*?)\}/g, "")
-        .trim();
-
-      if (idText) {
-        el.setAttribute("id", idText);
-      }
-
-      el.textContent = newTextContent;
-    });
-
-    const htmlString = dom.window.document.body.innerHTML;
-
-    return htmlString;
+    return applyIdsToElements(html);
   },
 };
