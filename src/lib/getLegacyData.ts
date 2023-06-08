8c42266874ab96040dafe8bea606814914ed0240
Nathan Arthur <nathan@nathanarthur.com>
2023-06-08 14:45:00 -0400

    reduce build time

--- src/lib/getLegacyData.ts
+++ src/lib/getLegacyData.ts
@@ -1,5 +1,10 @@
+import fs from "fs";
+import { parse } from "csv-parse/sync";
+
 export default async function getLegacyData(): Promise<
   Array<Record<string, unknown>>
 > {
-  return (await import("../../wp-export.csv")).default;
+  return parse(fs.readFileSync("wp-export.csv", "utf-8"), {
+    columns: true,
+  });
 }
