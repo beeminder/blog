d19b749b34abdd248ea21b48856b7ef8012b5424
Nathan Arthur <nathan@nathanarthur.com>
2023-06-13 10:26:47 -0400

    configure rss feed

---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,24 @@
+import rss from "@astrojs/rss";
+import type { APIContext } from "astro";
+import getPosts from "../lib/getPosts";
+
+export async function get(context: APIContext) {
+  if (!context.site) {
+    throw new Error("Missing site metadata");
+  }
+
+  const posts = await getPosts();
+
+  return rss({
+    title: "Beeminder Blog",
+    description: "Beeminder Blog",
+    site: context.site.toString(),
+    items: posts.slice(0, 30).map((p) => ({
+      link: `${context.site}${p.slug}`,
+      title: p.title,
+      pubDate: p.date,
+      description: p.excerpt,
+      content: p.content,
+    })),
+  });
+}
