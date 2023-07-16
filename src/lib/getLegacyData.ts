ed70908d94db8be632da50e0183dd1cc01eda361
Nathan Arthur <nathan@nathanarthur.com>
2023-07-16 10:48:47 -0400

    legacy data validation

--- src/lib/getLegacyData.ts
+++ src/lib/getLegacyData.ts
@@ -1,7 +1,36 @@
 import loadLegacyData from "./loadLegacyData";
+import { z } from "zod";
+import memoize from "./memoize";
+
+const postSchema = z
+  .object({
+    ID: z.number(),
+    Title: z.string(),
+    Date: z.coerce.date(),
+    Tags: z.preprocess(
+      (val) => String(val).split("|").filter(Boolean),
+      z.array(z.string())
+    ),
+    expost_source_url: z.string(),
+    dsq_thread_id: z.number(),
+    "Author Username": z.string(),
+    Slug: z.string(),
+    "Comment Status": z.enum(["open", "closed"]),
+    "Ping Status": z.enum(["open", "closed"]),
+    "Post Modified Date": z.coerce.date(),
+    Status: z.enum(["publish", "pending", "draft"]),
+  })
+  .partial();
+
+const postsSchema = z.array(postSchema);
+
+const getParsedLegacyData = memoize(() => {
+  const raw = loadLegacyData();
+  return postsSchema.parse(raw);
+}, "getParsedLegacyData");
 
 export default function getLegacyData(
   url: string
-): Record<string, unknown> | undefined {
-  return loadLegacyData().find((p) => p.expost_source_url === url);
+): z.infer<typeof postSchema> | undefined {
+  return getParsedLegacyData().find((p) => p.expost_source_url === url);
 }
