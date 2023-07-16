8a3c2297793e91332bf7d7262091f3005f9a5d49
Nathan Arthur <nathan@nathanarthur.com>
2023-07-16 16:09:02 -0400

    terse code

--- src/lib/fetchPost.ts
+++ src/lib/fetchPost.ts
@@ -3,6 +3,5 @@ import { fetchBuilder, FileSystemCache } from "node-fetch-cache";
 const fetch = fetchBuilder.withCache(new FileSystemCache());
 
 export default async function fetchPost(url: string): Promise<string> {
-  const r = await fetch(`${url}/export/txt`);
-  return await r.text();
+  return fetch(`${url}/export/txt`).then((r) => r.text());
 }
