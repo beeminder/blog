0ac2b54b022e7883c474b05e1e4bc18b40258a88
Nathan Arthur <nathan@nathanarthur.com>
2023-08-10 09:35:56 -0400

    adjust caching based on environment

--- src/lib/fetchPost.ts
+++ src/lib/fetchPost.ts
@@ -1,6 +1,7 @@
-import { fetchBuilder, FileSystemCache } from "node-fetch-cache";
+import { fetchBuilder, FileSystemCache, MemoryCache } from "node-fetch-cache";
 
-const fetch = fetchBuilder.withCache(new FileSystemCache());
+const cache = process.env.RENDER ? new MemoryCache() : new FileSystemCache();
+const fetch = fetchBuilder.withCache(cache);
 
 export default async function fetchPost(url: string): Promise<string> {
   return fetch(`${url}/export/txt`).then((r) => r.text());
