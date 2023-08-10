17f110eb0bc5ad70be0d068e3bb4fec20de89580
Nathan Arthur <nathan@nathanarthur.com>
2023-08-10 09:13:10 -0400

    image titles

--- src/schemas/image.ts
+++ src/schemas/image.ts
@@ -6,29 +6,35 @@ export type Image = {
   extracted?: boolean | undefined;
 } & HTMLAttributes<"img">;
 
-export const image: z.ZodType<Image> = z.object({
-  src: z.string(),
-  extracted: z.boolean().optional(),
-  alt: z.string().optional(),
-  crossorigin: z.enum(["anonymous", "use-credentials"]).optional(),
-  decoding: z.enum(["sync", "async", "auto"]).optional(),
-  fetchpriority: z.enum(["auto", "high", "low"]).optional(),
-  height: z.number().optional(),
-  loading: z.enum(["eager", "lazy"]).optional(),
-  referrerpolicy: z
-    .enum([
-      "no-referrer",
-      "no-referrer-when-downgrade",
-      "origin",
-      "origin-when-cross-origin",
-      "same-origin",
-      "strict-origin",
-      "strict-origin-when-cross-origin",
-      "unsafe-url",
-    ])
-    .optional(),
-  sizes: z.string().optional(),
-  srcset: z.string().optional(),
-  usemap: z.string().optional(),
-  width: z.number().optional(),
-});
+export const image: z.ZodType<Image> = z
+  .object({
+    src: z.string(),
+    extracted: z.boolean().optional(),
+    alt: z.string().optional(),
+    title: z.string().optional(),
+    crossorigin: z.enum(["anonymous", "use-credentials"]).optional(),
+    decoding: z.enum(["sync", "async", "auto"]).optional(),
+    fetchpriority: z.enum(["auto", "high", "low"]).optional(),
+    height: z.number().optional(),
+    loading: z.enum(["eager", "lazy"]).optional(),
+    referrerpolicy: z
+      .enum([
+        "no-referrer",
+        "no-referrer-when-downgrade",
+        "origin",
+        "origin-when-cross-origin",
+        "same-origin",
+        "strict-origin",
+        "strict-origin-when-cross-origin",
+        "unsafe-url",
+      ])
+      .optional(),
+    sizes: z.string().optional(),
+    srcset: z.string().optional(),
+    usemap: z.string().optional(),
+    width: z.number().optional(),
+  })
+  .transform((img) => ({
+    ...img,
+    title: img.title || img.alt,
+  }));
