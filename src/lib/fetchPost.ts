f9ca73cc1b9fb13aa213cf1747a2e7858f20de29
Nathan Arthur <nathan@nathanarthur.com>
2023-08-12 17:31:52 -0400

    log caching strategy selection

--- src/lib/fetchPost.ts
+++ src/lib/fetchPost.ts
@@ -1,10 +1,17 @@
 import { fetchBuilder, FileSystemCache, MemoryCache } from "node-fetch-cache";
 import formatEtherpadUrl from "./formatEtherpadUrl";
 
-const cache =
-  import.meta.env.RENDER || import.meta.env.FILE_SYSTEM_CACHE === "false"
-    ? new MemoryCache()
-    : new FileSystemCache();
+function buildCache() {
+  if (import.meta.env.RENDER || import.meta.env.FILE_SYSTEM_CACHE === "false") {
+    console.info("Using in-memory cache");
+    return new MemoryCache();
+  } else {
+    console.info("Using file-system cache");
+    return new FileSystemCache();
+  }
+}
+
+const cache = buildCache();
 const fetch = fetchBuilder.withCache(cache);
 
 export default async function fetchPost(url: string): Promise<string> {
