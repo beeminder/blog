c8f36f68a8d2ccba672766c75082055bf2acd4a1
Nathan Arthur <nathan@nathanarthur.com>
2023-08-12 17:28:29 -0400

    etherpad fetch concurrency

--- src/lib/fetchPost.ts
+++ src/lib/fetchPost.ts
@@ -1,7 +1,10 @@
 import { fetchBuilder, FileSystemCache, MemoryCache } from "node-fetch-cache";
 import formatEtherpadUrl from "./formatEtherpadUrl";
 
-const cache = process.env.RENDER ? new MemoryCache() : new FileSystemCache();
+const cache =
+  import.meta.env.RENDER || import.meta.env.FILE_SYSTEM_CACHE === "false"
+    ? new MemoryCache()
+    : new FileSystemCache();
 const fetch = fetchBuilder.withCache(cache);
 
 export default async function fetchPost(url: string): Promise<string> {
--- src/lib/fetchPosts.ts
+++ src/lib/fetchPosts.ts
@@ -1,18 +1,21 @@
 import type { PostInput } from "../schemas/post";
 import fetchPost from "./fetchPost";
 import readSources from "./readSources";
+import pLimit from "p-limit";
+
+async function get(url: string): Promise<PostInput> {
+  return {
+    url,
+    md: await fetchPost(url),
+  };
+}
 
 export default async function fetchPosts(): Promise<PostInput[]> {
   const urls = readSources();
-  const inputs: PostInput[] = [];
+  const limit = pLimit(10);
 
   console.time("Fetching markdown from Etherpad");
-  for (const url of urls) {
-    inputs.push({
-      url,
-      md: await fetchPost(url),
-    });
-  }
+  const inputs = await Promise.all(urls.map((url) => limit(get, url)));
   console.timeEnd("Fetching markdown from Etherpad");
 
   return inputs;
