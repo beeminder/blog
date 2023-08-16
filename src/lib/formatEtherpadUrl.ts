4efab08ab95f577d1482e3eb6a32ec77435b705d
Daniel Reeves <dreeves@beeminder.com>
2023-08-15 23:00:02 -0700

    Canonicalize URLs

--- src/lib/formatEtherpadUrl.ts
+++ src/lib/formatEtherpadUrl.ts
@@ -1,5 +1,12 @@
-export default function formatEtherpadUrl(url: string): string {
-  url = url.replace("dtherpad.com", "<etherpad-host>");
+export default function canonicalizeUrl(url: string): string {
+  url = url.trim();
   url = url.startsWith("http") ? url : `https://${url}`;
+  // TODO: assert that the url now starts with http:// or https://
+  // we want to fail loudly here if the URL is mangled somehow
+  url = url.replace(/\/+$/, ''); // kill trailing slashes
+  // following is a special case for the raw <etherpad-host> URL just in case
+  url = url.replace(/^(https?:\/\/padm\.us)$/, "$1/public/export/txt");
+  // following is the key transformation: append /export/txt to the URL
+  url = url.replace(/^(https?:\/\/padm\.us)\/(.+)$/, "$1/$2/export/txt");
   return url;
 }
