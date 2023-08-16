7d3c0caf0471b865c1574862f01c4409adcbfb75
Nathan Arthur <nathan@nathanarthur.com>
2023-08-16 16:11:53 -0400

    code formatting

--- src/lib/canonicalizeUrl.ts
+++ src/lib/canonicalizeUrl.ts
@@ -3,7 +3,7 @@ export default function canonicalizeUrl(url: string): string {
   url = url.startsWith("http") ? url : `https://${url}`;
   // TODO: assert that the url now starts with http:// or https://
   // we want to fail loudly here if the URL is mangled somehow
-  url = url.replace(/\/+$/, ''); // kill trailing slashes
+  url = url.replace(/\/+$/, ""); // kill trailing slashes
   // following is a special case for the raw padm.us URL just in case
   url = url.replace(/^(https?:\/\/padm\.us)$/, "$1/public/export/txt");
   // following is the key transformation: append /export/txt to the URL
