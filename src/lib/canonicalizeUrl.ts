fc9091913c2b7273640dbc87444fafecf0efc0a2
Daniel Reeves <dreeves@beeminder.com>
2023-08-16 15:44:22 -0700

    Syntactic sugar for etherpad URLs

--- src/lib/canonicalizeUrl.ts
+++ src/lib/canonicalizeUrl.ts
@@ -1,12 +1,24 @@
 export default function canonicalizeUrl(url: string): string {
-  url = url.trim();
-  url = url.startsWith("http") ? url : `https://${url}`;
-  // TODO: assert that the url now starts with http:// or https://
-  // we want to fail loudly here if the URL is mangled somehow
+  // Start with basic cleanup canonicalization applicable to all URLs...
+  url = url.trim(); // trim whitespace
+  if (!url.match(/^https?:\/\//i)) url = "https://" + url // add protocol
   url = url.replace(/\/+$/, ""); // kill trailing slashes
-  // following is a special case for the raw padm.us URL just in case
+  // TODO: fail loudly if the URL is mangled somehow
+
+  // Now handle syntactic sugar for etherpad URLs:
+  const sugarlist = [
+    /^(https?:\/\/etherpad)\/([^/]+)$/,
+    /^(https?:\/\/doc\.bmndr\.co)\/([^/]+)$/,
+    /^(https?:\/\/doc\.bmndr\.com)\/([^/]+)$/,
+    /^(https?:\/\/doc\.beeminder\.com)\/([^/]+)$/,
+  ];
+  for (const regex of sugarlist) {
+    url = url.replace(regex, "https://" + "padm" + "." + "us" + "/" + "$2");
+  }
+
+  // Following is a special case for the raw padm.us URL just in case:
   url = url.replace(/^(https?:\/\/padm\.us)$/, "$1/public/export/txt");
-  // following is the key transformation: append /export/txt to the URL
-  url = url.replace(/^(https?:\/\/padm\.us)\/([^/]+)$/, "$1/$2/export/txt");
+  // And finally the key transformation: append /export/txt for etherpad:
+  url = url.replace(/^(https?:\/\/padm\.us)\/([^\/]+)$/, "$1/$2/export/txt");
   return url;
 }
