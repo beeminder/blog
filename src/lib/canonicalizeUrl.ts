a78902389f10d02ddd637020fa5186f632e1f671
Daniel Reeves <dreeves@beeminder.com>
2023-08-15 23:31:44 -0700

    Spaghetti, meet wall

--- src/lib/canonicalizeUrl.ts
+++ src/lib/canonicalizeUrl.ts
@@ -7,6 +7,6 @@ export default function canonicalizeUrl(url: string): string {
   // following is a special case for the raw padm.us URL just in case
   url = url.replace(/^(https?:\/\/padm\.us)$/, "$1/public/export/txt");
   // following is the key transformation: append /export/txt to the URL
-  url = url.replace(/^(https?:\/\/padm\.us)\/(.+)$/, "$1/$2/export/txt");
+  url = url.replace(/^(https?:\/\/padm\.us)\/([^\/]+)$/, "$1/$2/export/txt");
   return url;
 }
--- src/lib/fetchPosts.ts
+++ src/lib/fetchPosts.ts
@@ -2,6 +2,8 @@ import type { PostInput } from "../schemas/post";
 import fetchPost from "./fetchPost";
 import readSources from "./readSources";
 import pLimit from "p-limit";
+import canonicalizeUrl from "./canonicalizeUrl";
+
 
 async function get(url: string): Promise<PostInput> {
   return {
@@ -12,5 +14,5 @@ async function get(url: string): Promise<PostInput> {
 
 export default function fetchPosts(): Promise<PostInput>[] {
   const l = pLimit(10);
-  return readSources().map((url) => l(get, url));
+  return readSources().map((url) => l(get, canonicalizeUrl(url)));
 }
