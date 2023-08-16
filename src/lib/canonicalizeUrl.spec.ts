8f552a71e5b6c5e056134b8b26d6241e42d1a967
Nathan Arthur <nathan@nathanarthur.com>
2023-08-16 16:10:36 -0400

    clean up test output

--- src/lib/canonicalizeUrl.spec.ts
+++ src/lib/canonicalizeUrl.spec.ts
@@ -3,16 +3,8 @@ import canonicalizeUrl from "./canonicalizeUrl";
 
 describe("canonicalizeUrl", () => {
   it("adds schema if missing", async () => {
-    const result = canonicalizeUrl("dtherpad.com/psychpricing");
+    const result = canonicalizeUrl("padm.us/psychpricing");
 
     expect(result).toContain("https://");
   });
-
-  it("works correctly TODO better name", async () => {
-    const result = canonicalizeUrl("https://padm.us/beemblog-akrasia/export/txt");
-    console.log(result);
-    expect(result).toContain("https://");
-  });
-
 });
-
