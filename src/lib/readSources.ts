599f8169602ad9432018df57051f58a7017abf84
Daniel Reeves <dreeves@beeminder.com>
2023-08-16 00:00:24 -0700

    Try canonicalizing off the bat

--- src/lib/readSources.ts
+++ src/lib/readSources.ts
@@ -1,5 +1,7 @@
 import fs from "fs";
+import canonicalizeUrl from "./canonicalizeUrl";
 
 export default function readSources(): string[] {
-  return fs.readFileSync("sources.txt", "utf-8").split("\n").filter(Boolean);
+  return fs.readFileSync("sources.txt", "utf-8")
+    .split("\n").filter(Boolean).map(canonicalizeUrl);
 }
