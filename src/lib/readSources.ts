6fe8bdcd636b027bb883a3c964dd0f4e9487cf40
Nathan Arthur <nathan@nathanarthur.com>
2023-08-16 16:11:53 -0400

    code formatting

--- src/lib/readSources.ts
+++ src/lib/readSources.ts
@@ -2,6 +2,9 @@ import fs from "fs";
 import canonicalizeUrl from "./canonicalizeUrl";
 
 export default function readSources(): string[] {
-  return fs.readFileSync("sources.txt", "utf-8")
-    .split("\n").filter(Boolean).map(canonicalizeUrl);
+  return fs
+    .readFileSync("sources.txt", "utf-8")
+    .split("\n")
+    .filter(Boolean)
+    .map(canonicalizeUrl);
 }
