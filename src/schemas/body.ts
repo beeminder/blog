3504bb3623381888a8b9a5ce8d0181a98d628674
Nathan Arthur <nathan@nathanarthur.com>
2023-08-19 11:06:38 -0400

    fix em dashes

--- src/schemas/body.ts
+++ src/schemas/body.ts
@@ -12,7 +12,11 @@ const MARKED_OPTIONS = {
   headerIds: false,
 } as const;
 
-marked.use(markedSmartypants());
+marked.use(
+  markedSmartypants({
+    config: "1",
+  }),
+);
 marked.use({
   hooks: {
     postprocess: applyIdsToElements,
