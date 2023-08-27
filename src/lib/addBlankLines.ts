27435ea9715aa3e671e1b5e6f2be14464c850385
Nathan Arthur <nathan@nathanarthur.com>
2023-08-27 16:50:20 -0400

    add todo comment

--- src/lib/addBlankLines.ts
+++ src/lib/addBlankLines.ts
@@ -1,4 +1,4 @@
-// WORKAROUND:
+// TODO: https://github.com/beeminder/blog/issues/354
 const blockElements =
   "address|article|aside|blockquote|canvas|dd|div|dl|dt|fieldset|figcaption|figure|footer|form|h\\d|header|hr|li|main|nav|noscript|ol|output|p|pre|section|table|tfoot|ul|video";
 const regex = new RegExp(`(</?(${blockElements})[^>]*>)\n+`, "g");
