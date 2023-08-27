a87d9357e90ccf12cddd22952329a0890d79ca13
Nathan Arthur <nathan@nathanarthur.com>
2023-08-27 16:57:58 -0400

    output is not block level

--- src/lib/addBlankLines.ts
+++ src/lib/addBlankLines.ts
@@ -1,6 +1,6 @@
 // TODO: https://github.com/beeminder/blog/issues/354
 const blockElements =
-  "address|article|aside|blockquote|canvas|dd|div|dl|dt|fieldset|figcaption|figure|footer|form|h\\d|header|hr|li|main|nav|noscript|ol|output|p|pre|section|table|tfoot|ul|video";
+  "address|article|aside|blockquote|canvas|dd|div|dl|dt|fieldset|figcaption|figure|footer|form|h\\d|header|hr|li|main|nav|noscript|ol|p|pre|section|table|tfoot|ul|video";
 const regex = new RegExp(`(</?(${blockElements})[^>]*>)\n+`, "g");
 
 export default function addBlankLines(markdown: string): string {
