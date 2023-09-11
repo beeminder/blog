bfa0bae750fdd8d52948a5e4d9617ef25b6fdbeb
Nathan Arthur <nathan@nathanarthur.com>
2023-09-11 15:48:10 -0400

    require magic tags

--- src/lib/getExcerpt.spec.ts
+++ src/lib/getExcerpt.spec.ts
@@ -1,24 +1,33 @@
 import getExcerpt from "./getExcerpt";
 import { describe, it, expect } from "vitest";
+import padm from "./test/padm";
 
 const lorem =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
 describe("getExcerpt", () => {
   it("appends ellipsis to excerpt", () => {
-    const result = getExcerpt("");
+    const result = getExcerpt(padm());
 
     expect(result).toBe("...");
   });
 
   it("does not include images in excerpt", () => {
-    const result = getExcerpt('<img src="https://example.com/image.png" />');
+    const result = getExcerpt(
+      padm({
+        content: '<img src="https://example.com/image.png" />',
+      }),
+    );
 
     expect(result).toBe("...");
   });
 
   it("limits length fuzzily", () => {
-    const result = getExcerpt(lorem);
+    const result = getExcerpt(
+      padm({
+        content: lorem,
+      }),
+    );
 
     const words = result.split(" ");
 
@@ -26,21 +35,31 @@ describe("getExcerpt", () => {
   });
 
   it("strips footnotes", () => {
-    const result = getExcerpt('<a class="footnote" id="fn1" href="#1">1</a>');
+    const result = getExcerpt(
+      padm({
+        content: '<a class="footnote" id="fn1" href="#1">1</a>',
+      }),
+    );
 
     expect(result).toBe("...");
   });
 
   it("strips newlines", () => {
-    const result = getExcerpt("foo\nbar");
+    const result = getExcerpt(
+      padm({
+        content: "foo\nbar",
+      }),
+    );
 
     expect(result).toBe("foo bar...");
   });
 
   it("does not include private notes", () => {
-    const result = getExcerpt(`private notes
-BEGIN_MAGIC
-content`);
+    const result = getExcerpt(
+      padm({
+        before: "private notes",
+      }),
+    );
 
     expect(result).not.toContain("private notes");
   });
--- src/schemas/body.spec.ts
+++ src/schemas/body.spec.ts
@@ -1,11 +1,24 @@
 import { describe, it, expect } from "vitest";
 import { body } from "./body";
+import padm from "../lib/test/padm";
 
 describe("body", () => {
   it("supports em dashes", () => {
-    const r = body.parse("foo -- bar");
+    const r = body.parse(
+      padm({
+        content: "foo -- bar",
+      }),
+    );
 
     // https://www.codetable.net/name/em-dash
     expect(r).toContain("&#8212;");
+  });
+
+  it("throws if no BEGIN_MAGIC", () => {
+    expect(() => body.parse("hello world\nEND_MAGIC")).toThrow();
+  });
+
+  it("throws if no END_MAGIC", () => {
+    expect(() => body.parse("BEGIN_MAGIC\nhello world")).toThrow();
   });
 });
