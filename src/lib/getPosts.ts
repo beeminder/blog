1b7e756a36bfb30a250bca7a6dc5455b56407df2
Nathan Arthur <nathan@nathanarthur.com>
2023-09-11 14:52:39 -0400

    enforce slug and disqus id uniqueness

--- src/lib/getPosts.ts
+++ src/lib/getPosts.ts
@@ -2,6 +2,14 @@ import memoize from "./memoize";
 import { type Post, post } from "../schemas/post";
 import fetchPosts from "./fetchPosts";
 
+const getDuplicates = (
+  arr: Array<Record<string, unknown>>,
+  key: string,
+): unknown[] => {
+  const keys = arr.map((item) => item[key]);
+  return keys.filter((key) => keys.indexOf(key) !== keys.lastIndexOf(key));
+};
+
 const makePosts = memoize((): Promise<Post>[] =>
   fetchPosts().map((p) =>
     p.then((d) => {
@@ -24,6 +32,24 @@ const getPosts = memoize(
     sort?: boolean;
   } = {}): Promise<Post[]> => {
     const posts = await Promise.all(makePosts());
+
+    const duplicateSlugs = getDuplicates(posts, "slug");
+    if (duplicateSlugs.length > 0) {
+      throw new Error(
+        `Duplicate slugs found: ${duplicateSlugs.join(
+          ", ",
+        )}. Slugs must be unique.`,
+      );
+    }
+
+    const duplicateDisqusIds = getDuplicates(posts, "disqus_id");
+    if (duplicateDisqusIds.length > 0) {
+      throw new Error(
+        `Duplicate disqus_ids found: ${duplicateDisqusIds.join(
+          ", ",
+        )}. Disqus_ids must be unique.`,
+      );
+    }
 
     if (sort) {
       posts.sort((a, b) => b.date.getTime() - a.date.getTime());
