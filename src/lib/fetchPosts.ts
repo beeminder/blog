4edde45114378517fd68ddc466664a247ac072a9
Nathan Arthur <nathan@nathanarthur.com>
2023-09-16 16:14:03 -0400

    first pass at unified posts json

--- src/lib/fetchPosts.ts
+++ src/lib/fetchPosts.ts
@@ -1,16 +1,22 @@
 import type { PostInput } from "../schemas/post";
+import canonicalizeUrl from "./canonicalizeUrl";
 import fetchPost from "./fetchPost";
 import readSources from "./readSources";
 import pLimit from "p-limit";
 
-async function get(url: string): Promise<PostInput> {
+async function get(post: Record<string, unknown>): Promise<PostInput> {
+  if (!post.source) throw new Error(`Missing url in ${JSON.stringify(post)}`);
+
+  const url = canonicalizeUrl(post.source.toString());
   return {
-    url,
+    ...(post as PostInput),
     md: await fetchPost(url),
   };
 }
 
 export default function fetchPosts(): Promise<PostInput>[] {
   const l = pLimit(10);
-  return readSources().map((url) => l(get, url));
+  return readSources().map((s) => {
+    return l(get, s);
+  });
 }
