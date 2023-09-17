f1a541492ff3363761ac9fdba8b603aedcfb552d
Nathan Arthur <nathan@nathanarthur.com>
2023-09-17 10:39:34 -0400

    fix tests

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig, sharpImageService } from "astro/config";
 import prefetch from "@astrojs/prefetch";
-import getPosts from "./src/lib/getPosts";
-
-const posts = await getPosts();
-const redirects = posts.reduce<Record<string, string>>((acc, post) => {
-  if (!post.redirects.length) return acc;
-  post.redirects.forEach((r) => {
-    acc[`/${r}`] = `/${post.slug}`;
-  });
-  return acc;
-}, {});
+import getRedirects from "./src/lib/getRedirects";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,7 +9,7 @@ export default defineConfig({
     service: sharpImageService(),
     domains: ["blog.beeminder.com", "user-images.githubusercontent.com"],
   },
-  redirects,
+  redirects: await getRedirects(),
   integrations: [
     prefetch({
       selector: "a",
