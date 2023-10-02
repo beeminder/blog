a00f241c7d0bdfaac9d04e81fbe96469fdc4a3cb
Nathan Arthur <nathan@nathanarthur.com>
2023-10-02 11:19:21 -0400

    reduce snapshot test log noise
    Co-authored-by: lcarthurscam@gmail.com <lcarthurscam@gmail.com>

--- vitest.snapshot.config.ts
+++ vitest.snapshot.config.ts
@@ -1,11 +1,14 @@
 import config from "./vitest.config";
+import Reporter from "./vitest.snapshot.reporter";
+import { defineConfig } from "vitest/config";
 
-export default {
+export default defineConfig({
   ...config,
   test: {
     ...config.test,
     setupFiles: [],
     include: ["src/**/*.spec-snapshot.ts"],
     watch: false,
+    reporters: new Reporter(),
   },
-};
+});
