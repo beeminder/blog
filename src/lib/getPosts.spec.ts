7bc9e93cbbe429cdc7cabd4290315136022b526b
lcflight <lcarthur747@gmail.com>
2023-10-20 10:46:11 -0500

    sanatize-html

--- src/lib/getPosts.spec.ts
+++ src/lib/getPosts.spec.ts
@@ -359,7 +359,7 @@ describe("getPosts", () => {
     const posts = await getPosts();
     const { content } = posts.find((p) => p.slug === "psychpricing") || {};
 
-    expect(content).toContain("&#8220;hello world&#8221;");
+    expect(content).toContain("“hello world”");
   });
 
   it("does not require new line after html element", async () => {
