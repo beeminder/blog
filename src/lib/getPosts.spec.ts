ab3464dce8311f17db19c960cdc9c03dfe42ef11
lcflight <lcarthur747@gmail.com>
2023-10-20 11:16:44 -0500

    tests update

--- src/lib/getPosts.spec.ts
+++ src/lib/getPosts.spec.ts
@@ -359,7 +359,7 @@ describe("getPosts", () => {
     const posts = await getPosts();
     const { content } = posts.find((p) => p.slug === "psychpricing") || {};
 
-    expect(content).toContain("“hello world”");
+    expect(content).toContain("&#8220;hello world&#8221;");
   });
 
   it("does not require new line after html element", async () => {
