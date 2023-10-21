7c217cb3cdd1a5210d7618b9504695cad71fd27a
Nathan Arthur <nathan@nathanarthur.com>
2023-10-21 11:25:17 -0400

    add meta test helper

--- src/lib/getArchives.spec.ts
+++ src/lib/getArchives.spec.ts
@@ -1,29 +1,11 @@
 import { describe, it, expect, beforeEach, vi } from "vitest";
 import getArchives from "./getArchives";
 import readSources from "./readSources";
+import meta from "./test/meta";
 
 describe("getArchives", () => {
   beforeEach(() => {
-    vi.mocked(readSources).mockReturnValue([
-      {
-        title: "Test Post",
-        source: "https://padm.us/psychpricing",
-        date: "2011-01-24",
-        slug: "psychpricing",
-        status: "publish",
-        author: "author",
-        excerpt: "excerpt",
-        disqus_id: "abc",
-        redirects: [],
-        tags: [],
-      },
-    ]);
-  });
-
-  it("gets archives", async () => {
-    const result = await getArchives();
-
-    expect(result[0]).toBeDefined();
+    vi.mocked(readSources).mockReturnValue([meta()]);
   });
 
   it("batches by month", async () => {
@@ -40,30 +22,8 @@ describe("getArchives", () => {
 
   it("properly batches by year", async () => {
     vi.mocked(readSources).mockReturnValue([
-      {
-        title: "Test Post",
-        source: "https://padm.us/psychpricing",
-        date: "2013-02-22",
-        slug: "psychpricing",
-        status: "publish",
-        author: "author",
-        disqus_id: "abc",
-        redirects: [],
-        tags: [],
-        excerpt: "MAGIC_AUTO_EXTRACT",
-      },
-      {
-        title: "Second Post",
-        source: "https://padm.us/second",
-        date: "2015-02-22",
-        slug: "second",
-        status: "publish",
-        author: "author",
-        disqus_id: "def",
-        redirects: [],
-        tags: [],
-        excerpt: "MAGIC_AUTO_EXTRACT",
-      },
+      meta({ date: "2013-02-22" }),
+      meta({ date: "2015-02-22" }),
     ]);
 
     const result = await getArchives();
@@ -72,44 +32,7 @@ describe("getArchives", () => {
   });
 
   it("handles three posts in one month", async () => {
-    vi.mocked(readSources).mockReturnValue([
-      {
-        title: "A",
-        source: "https://padm.us/a",
-        date: "2013-02-22",
-        slug: "a",
-        status: "publish",
-        author: "author",
-        disqus_id: "a",
-        redirects: [],
-        tags: [],
-        excerpt: "MAGIC_AUTO_EXTRACT",
-      },
-      {
-        title: "B",
-        source: "https://padm.us/b",
-        date: "2013-02-22",
-        slug: "b",
-        status: "publish",
-        author: "author",
-        disqus_id: "b",
-        redirects: [],
-        tags: [],
-        excerpt: "MAGIC_AUTO_EXTRACT",
-      },
-      {
-        title: "C",
-        source: "https://padm.us/c",
-        date: "2013-02-22",
-        slug: "c",
-        status: "publish",
-        author: "author",
-        disqus_id: "c",
-        redirects: [],
-        tags: [],
-        excerpt: "MAGIC_AUTO_EXTRACT",
-      },
-    ]);
+    vi.mocked(readSources).mockReturnValue([meta(), meta(), meta()]);
 
     const result = await getArchives();
 
@@ -118,30 +41,8 @@ describe("getArchives", () => {
 
   it("sorts posts by date", async () => {
     vi.mocked(readSources).mockReturnValue([
-      {
-        title: "A",
-        source: "https://padm.us/a",
-        date: "2013-02-22",
-        slug: "a",
-        status: "publish",
-        author: "author",
-        disqus_id: "a",
-        redirects: [],
-        tags: [],
-        excerpt: "MAGIC_AUTO_EXTRACT",
-      },
-      {
-        title: "B",
-        source: "https://padm.us/b",
-        date: "2013-02-21",
-        slug: "b",
-        status: "publish",
-        author: "author",
-        disqus_id: "b",
-        redirects: [],
-        tags: [],
-        excerpt: "MAGIC_AUTO_EXTRACT",
-      },
+      meta({ title: "A", date: "2013-02-22" }),
+      meta({ title: "B", date: "2013-02-21" }),
     ]);
 
     const result = await getArchives();
