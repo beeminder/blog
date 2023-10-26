0ac72ea5c4a9684ffc1b53f7cac7967cbad22da8
lcflight <lcarthur747@gmail.com>
2023-10-26 13:28:55 -0500

    work on fixing puppeteer

--- src/lib/test/getSitemap.ts
+++ src/lib/test/getSitemap.ts
@@ -1,9 +1,12 @@
 export default async function getSitemap(): Promise<string[]> {
   console.log("getSitemap");
 
-  const xml: string = await fetch(`http://localhost:4321/sitemap-0.xml`).then(
-    (r) => r.text(),
-  );
+  const response = await fetch(`http://localhost:4321/sitemap-0.xml`);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch sitemap: ${response.statusText}`);
+  }
+  const xml = await response.text();
 
+  console.log(xml);
   return xml.match(/<loc>(.*?)<\/loc>/g)?.map((url) => url.slice(5, -6)) ?? [];
 }
