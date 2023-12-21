5405a7eab45f8991f5d2965a91dde8ca9afd2b15
lcflight <lcarthur747@gmail.com>
2023-12-21 10:05:14 -0600

    canonicalizeUrl Refactoring

--- src/lib/canonicalizeUrl.spec.ts
+++ src/lib/canonicalizeUrl.spec.ts
@@ -27,4 +27,39 @@ describe("canonicalizeUrl", () => {
       "https://the_etherpad_domain/psychpricing/export/txt",
     );
   });
+
+  it("replaces etherpad domain", async () => {
+    const result = canonicalizeUrl("etherpad/psychpricing");
+
+    expect(result).toContain("the_etherpad_domain");
+  });
+
+  it("replaces doc.bmndr.com domain", async () => {
+    const result = canonicalizeUrl("doc.bmndr.com/psychpricing");
+
+    expect(result).toContain("the_etherpad_domain");
+  });
+
+  it("replaces doc.beeminder.com domain", async () => {
+    const result = canonicalizeUrl("doc.beeminder.com/psychpricing");
+
+    expect(result).toContain("the_etherpad_domain");
+  });
+
+  it("does not replace unknown domain", async () => {
+    const result = canonicalizeUrl("doc.unknown.com/psychpricing");
+
+    expect(result).not.toContain("the_etherpad_domain");
+  });
+
+  it("strips trailing slashes", async () => {
+    const result = canonicalizeUrl("doc.unknown.com/psychpricing/");
+    expect(result).not.toMatch(/\/$/);
+  });
+
+  it("does not add /export/txt to unknown domain", async () => {
+    const result = canonicalizeUrl("doc.unknown.com/psychpricing");
+
+    expect(result).not.toContain("/export/txt");
+  });
 });
--- src/lib/canonicalizeUrl.ts
+++ src/lib/canonicalizeUrl.ts
@@ -1,35 +1,29 @@
 import env from "./env";
 
+const DOMAINS = [
+  "etherpad",
+  "doc.bmndr.co",
+  "doc.bmndr.com",
+  "doc.beeminder.com",
+];
+
 export default function canonicalizeUrl(url: string): string {
   const domain = env("ETHERPAD_DOMAIN");
-  // Start with basic cleanup canonicalization applicable to all URLs...
-  url = url.trim(); // trim whitespace
-  if (!url.match(/^https?:\/\//i)) url = "https://" + url; // add protocol
-  url = url.replace(/\/+$/, ""); // kill trailing slashes
-  // TODO: fail loudly if the URL is mangled somehow
 
-  // Now handle syntactic sugar for etherpad URLs:
-  const sugarlist = [
-    /^(https?:\/\/etherpad)\/([^/]+)$/,
-    /^(https?:\/\/doc\.bmndr\.co)\/([^/]+)$/,
-    /^(https?:\/\/doc\.bmndr\.com)\/([^/]+)$/,
-    /^(https?:\/\/doc\.beeminder\.com)\/([^/]+)$/,
-  ];
+  if (!domain) throw new Error("ETHERPAD_DOMAIN is not set");
 
-  for (const regex of sugarlist) {
-    url = url.replace(regex, `https://${domain}/$2`);
+  url = url.trim();
+
+  if (!url.match(/^https?:\/\//i)) url = `https://${url}`;
+
+  const _url = new URL(url);
+
+  _url.pathname = _url.pathname.replace(/\/+$/, "");
+
+  if (DOMAINS.includes(_url.hostname)) {
+    _url.hostname = domain;
+    _url.pathname = `${_url.pathname}/export/txt`;
   }
 
-  // Following is a special case for the raw source URL just in case:
-  url = url.replace(
-    new RegExp(`^(https?://${domain})$`),
-    "$1/public/export/txt",
-  );
-  // And finally the key transformation: append /export/txt for etherpad:
-  url = url.replace(
-    new RegExp(`^(https?://${domain})/([^/]+)$`),
-    "$1/$2/export/txt",
-  );
-
-  return url;
+  return _url.toString();
 }
