09391c8271187149c9840889bf549e6e14ab8568
lcflight <lcarthur747@gmail.com>
2024-03-07 13:42:06 -0500

    fixes footnote links that have ids which are substrings of other ids

--- src/lib/linkFootnotes.ts
+++ src/lib/linkFootnotes.ts
@@ -13,7 +13,7 @@ export default function linkFootnotes(markdown: string): string {
   const ids = findIdentifiers(markdown);
 
   return ids.reduce((acc, id, i) => {
-    const regex = new RegExp(`\\$(?:FN\\[)?${id}\\]?`, "g");
+    const regex = new RegExp(`\\$(?:FN\\[)?${id}\\b`, "g");
     const chunks = acc.split(regex);
 
     return chunks.reduce((acc, chunk, j, arr) => {
@@ -23,8 +23,9 @@ export default function linkFootnotes(markdown: string): string {
       const isEndNote = j === arr.length - 1;
       const htmlId = isEndNote ? id : `${id}${j}`;
       const href = isEndNote ? `${id}1` : id;
+      const after = chunk[0] === "]" ? chunk.slice(1) : chunk;
 
-      return `${acc}<a class="footnote" id="${htmlId}" href="#${href}">${text}</a>${chunk}`;
+      return `${acc}<a class="footnote" id="${htmlId}" href="#${href}">${text}</a>${after}`;
     });
   }, markdown);
 }
